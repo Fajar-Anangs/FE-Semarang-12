f30d82bfda86747f0c172d52f73ce078f0713ca9
FajarPutra1604 <142969849+FajarPutra1604@users.noreply.github.com>
2023-10-05 18:09:20 +0700

    Update Tampilan

---
+++ js/kirim.js
@@ -0,0 +1,93 @@
+function sendForm(event) {
+    event.preventDefault(); // Mencegah perilaku default pengiriman formulir
+
+    const form = document.getElementById('contactForm');
+    const formData = {
+        nama: form.elements.nama.value,
+        email: form.elements.email.value,
+        subject: form.elements.subject.value,
+        pesan: form.elements.pesan.value
+    };
+
+    fetch('http://localhost:3001/api/contact', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => {
+        if (response.ok) {
+            // Handle a successful response (e.g., show a success message)
+            console.log('Form submitted successfully');
+        } else {
+            // Handle errors if the request fails
+            console.error('Form submission failed');
+        }
+    })
+    .catch(error => {
+        // Handle network or other errors
+        console.error('Error:', error);
+    });
+}
+
+document.getElementById('contactForm').addEventListener('submit', sendForm);
+
+function registerUser() {
+    // Mendapatkan nilai dari input fields
+    const nama = document.getElementById('nama').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    // Membuat objek JSON untuk data pengguna
+    const userData = {
+      nama: nama,
+      email: email,
+      password: password,
+      status: "Terkirim"
+    };
+  
+    // Mengirim data JSON ke backend menggunakan metode fetch
+    fetch('http://localhost:3001/api/registrasi', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle respon dari backend di sini, misalnya menampilkan pesan sukses atau kesalahan
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
+
+  function subscribeNewsletter() {
+    // Mendapatkan nilai dari input field email
+    const email = document.getElementById('email').value;
+  
+    // Membuat objek JSON untuk data berlangganan newsletter
+    const subscriptionData = {
+      email: email
+    };
+  
+    // Mengirim data JSON ke backend menggunakan metode fetch
+    fetch('http://localhost:3001/api/newsletter', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(subscriptionData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle respon dari backend di sini, misalnya menampilkan pesan sukses atau kesalahan
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
